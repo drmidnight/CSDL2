8b87aa30c0666ae24e0fb18114a56824e4a71304
Corey Morello <drmidnight@gmail.com>
2020-06-05 17:26:16 -0700

    added providers

--- Package.swift
+++ Package.swift
@@ -5,6 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "CSDL2",
+     providers: [
+    // .brew(["sqlite"]),
+        .apt([
+                "libsdl2-2.0-0",
+                "libsdl2-image-2.0-0",
+                "libsdl2-ttf-2.0-0"
+                ])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
